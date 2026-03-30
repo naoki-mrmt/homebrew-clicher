292ab8f8cf4fdc695290b89292b069075ef4f196
naoki-mrmt <n.bakupu@gmail.com>
2026-03-30 23:25:34 +0900

    chore: bump clicher to 0.0.2

--- Casks/clicher.rb
+++ Casks/clicher.rb
@@ -1,6 +1,6 @@
 cask "clicher" do
-  version "0.0.1"
-  sha256 "fb9036594eacf43d06ef9224bdfbb0c55fb4bfbc0ad6b34532a6d5f511782058"
+  version "0.0.2"
+  sha256 "bb5581ab0255fd1cfe8672b7e48ee54ce653955ffe961b531b77f5b7285a3552"
 
   url "https://github.com/naoki-mrmt/Clicher/releases/download/v#{version}/Clicher-#{version}.dmg"
   name "Clicher"

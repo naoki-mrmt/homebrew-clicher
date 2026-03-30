2c2b692636a0c732803b05e1517b55eb540c6ec1
naoki-mrmt <n.bakupu@gmail.com>
2026-03-31 00:18:25 +0900

    fix: update sha256 for notarized 0.0.3 dmg

--- Casks/clicher.rb
+++ Casks/clicher.rb
@@ -1,6 +1,6 @@
 cask "clicher" do
   version "0.0.3"
-  sha256 "3d24a098b91f7df2a26c56bea667eee22eccf5a6b889911050bcd1992658541f"
+  sha256 "ae8cf1a4979e3a390ab55ab19c68310cf7bd5efdf57369d60717664581045cb2"
 
   url "https://github.com/naoki-mrmt/Clicher/releases/download/v#{version}/Clicher-#{version}.dmg"
   name "Clicher"

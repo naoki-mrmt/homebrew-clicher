20ed398ca79df7e579d7abe4cbff6b1904607308
naoki-mrmt <n.bakupu@gmail.com>
2026-03-30 22:37:06 +0900

    feat: add clicher cask v0.0.1

---
+++ Casks/clicher.rb
@@ -0,0 +1,19 @@
+cask "clicher" do
+  version "0.0.1"
+  sha256 "fb9036594eacf43d06ef9224bdfbb0c55fb4bfbc0ad6b34532a6d5f511782058"
+
+  url "https://github.com/naoki-mrmt/Clicher/releases/download/v#{version}/Clicher-#{version}.dmg"
+  name "Clicher"
+  desc "Screenshot and annotation tool for macOS"
+  homepage "https://github.com/naoki-mrmt/Clicher"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Clicher.app"
+
+  zap trash: [
+    "~/Library/Application Support/Clicher",
+    "~/Library/Preferences/com.naoki-mrmt.Clicher.plist",
+    "~/Library/Caches/com.naoki-mrmt.Clicher",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# homebrew-clicher
+
+Homebrew tap for [Clicher](https://github.com/naoki-mrmt/Clicher) - macOS screenshot & annotation tool.
+
+## Install
+
+```bash
+brew tap naoki-mrmt/clicher
+brew install --cask clicher
+```
+
+## Update
+
+```bash
+brew upgrade --cask clicher
+```
+
+## Uninstall
+
+```bash
+brew uninstall --cask clicher
+```
